7d39bdfd2b43357749dfb73a1d716727727b7a3c
NIX\hrytsko <roman.hrytsko@nixs.com>
2024-07-06 12:17:05 +0300

    controller gets fields data and component showsthe difference

--- force-app/main/default/lwc/easyCompare/easyCompare.js
+++ force-app/main/default/lwc/easyCompare/easyCompare.js
@@ -1,8 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import compareRecords from '@salesforce/apex/EasyCompareController.compareRecords';
 
 export default class EasyCompare extends LightningElement {
     leftInput;
+    @track leftColumnResult;
+    @track rightColumnResult;
+    leftTextAreaValue = '';
+    rightTextAreaValue = '';
     rightInput;
     result;
     error;
@@ -14,9 +18,30 @@ export default class EasyCompare extends LightningElement {
         this.compareRecords();
     }
 
+    generateValuesForTheColumn(comparisonResult){
+       let textAreaValue = '';
+
+        try{
+            for(const [key, value] of Object.entries(comparisonResult)) {
+                let stringValue = JSON.stringify(value)
+                textAreaValue += `${key}: ${stringValue} \n`
+            }
+        } catch(error) {
+            console.log(error);
+        }
+
+        return textAreaValue;
+    }
+
     async compareRecords() {
         try {
             this.result = await compareRecords({ recordId1: this.leftInput.value, recordId2: this.rightInput.value });
+            this.leftColumnResult = this.result[0][this.leftInput.value];
+            this.rightColumnResult = this.result[1][this.rightInput.value];
+
+            this.leftTextAreaValue = this.generateValuesForTheColumn(this.leftColumnResult);
+            this.rightTextAreaValue = this.generateValuesForTheColumn(this.rightColumnResult);
+            
             this.error = undefined;
         } catch (error) {
             this.error = error;
